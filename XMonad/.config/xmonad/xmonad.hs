d2c1f4de380429ac0c86fd410af9902546ca197a
Philippe Gouin <bering@ringlogic.com>
2024-10-26 19:20:44 -0400

    figured out the VS Code Save dialogs

--- XMonad/.config/xmonad/xmonad.hs
+++ XMonad/.config/xmonad/xmonad.hs
@@ -161,6 +161,8 @@ myManageHook = composeAll
                    , className =? "Io.elementary.desktop.agent-polkit" --> doCenterFloat
                    , className =? "spectacle"             --> doCenterFloat
                    , title     =? "Open File"             --> doCenterFloat
+                   , title     =? "Save As"               --> doCenterFloat
+                   , (className =? "Electron" <&&> title =? "Code - OSS") --> doCenterFloat
                    ]
 
 layoutColumns   = renamed [Replace "Columns"]
