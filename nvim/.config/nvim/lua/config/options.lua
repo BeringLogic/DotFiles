faec56102c95aef66fb55b8176d13abc8f2283b3
Philippe Gouin <bering@ringlogic.com>
2024-11-26 21:24:37 -0500

    use system clipboard as default register

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,8 @@
 
 vim.g.mapleader = " "
 
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.signcolumn = "yes"
@@ -21,5 +23,4 @@ vim.opt.smartcase = true
 --vim.opt.backspace = "indent, eol, start" -- allow backspace on indent, end of line or insert mode start position
 
 vim.opt.swapfile = false
-
 
