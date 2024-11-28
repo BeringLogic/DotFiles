bd1c39f2b124fa088a1b30eaf5944d01ac6483a8
Philippe Gouin <bering@ringlogic.com>
2024-11-27 20:49:26 -0500

    Auto-session using new config format

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,11 +1,15 @@
 return {
   "rmagatti/auto-session",
-  config = function()
-    require("auto-session").setup({
-      vim.keymap.set("n", "<Leader>s/", "<cmd>Autosession search<CR>", { desc="Auto-session Search" }),
-      vim.keymap.set("n", "<Leader>ss", "<cmd>SessionSave<CR>", { desc="Auto-session Save" }),
-      vim.keymap.set("n", "<Leader>sr", "<cmd>SessionRestore<CR>", { desc="Auto-session Restore" }),
-    })
-  end
+  lazy=false,
+
+  keys = {
+    { "<Leader>s/", "<cmd>Autosession search<CR>", desc="Auto-session Search" },
+    { "<Leader>ss", "<cmd>SessionSave<CR>", desc="Auto-session Save" },
+    { "<Leader>sr", "<cmd>SessionRestore<CR>", desc="Auto-session Restore" },
+  },
+
+  opts = {
+    log_level="debug"
+  }
 }
 
