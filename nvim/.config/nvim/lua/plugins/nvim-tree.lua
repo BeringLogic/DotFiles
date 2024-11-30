63213a43fbbb07aa0ebb00420a3cb11a069cfe10
Philippe Gouin <bering@ringlogic.com>
2024-11-30 15:10:53 -0500

    formatting

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -27,7 +27,7 @@ return {
 			on_attach = function(bufnr)
 				local api = require("nvim-tree.api")
 
-        api.config.mappings.default_on_attach(bufnr)
+				api.config.mappings.default_on_attach(bufnr)
 
 				local lefty = function()
 					local node_at_cursor = api.tree.get_node_under_cursor()
@@ -58,12 +58,7 @@ return {
 		})
 
 		vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-		vim.keymap.set(
-			"n",
-			"<leader>ef",
-			"<cmd>NvimTreeFindFileToggle<CR>",
-			{ desc = "Toggle file explorer on current file" }
-		)
+		vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
 		vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
 		vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 	end,
