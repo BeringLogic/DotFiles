bdbdf95794086367b6ae2e2b3e96540726fbdc45
Philippe Gouin <bering@ringlogic.com>
2024-11-30 10:54:46 -0500

    added cmp-nvim-lsp to auto-complete lsp stuff

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -18,9 +18,15 @@ return {
     lazy = false,
 
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.pylsp.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pylsp.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc="Hover" })
       vim.keymap.set("n", "<F12>", vim.lsp.buf.definition, { desc="Definitions" })
