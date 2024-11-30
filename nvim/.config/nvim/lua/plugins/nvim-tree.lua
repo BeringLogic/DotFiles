0e2a71258637016a35954ed8dd25ac042703d7ce
Philippe Gouin <bering@ringlogic.com>
2024-11-30 15:00:35 -0500

    left and right fold and unfold folders

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,35 +1,70 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
- 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
-      -- disable window_picker for explorer to work well with window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      git = {
-        ignore = false,
-      },
-    })
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+			-- disable window_picker for explorer to work well with window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			git = {
+				ignore = false,
+			},
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+        api.config.mappings.default_on_attach(bufnr)
+
+				local lefty = function()
+					local node_at_cursor = api.tree.get_node_under_cursor()
+					-- node_at_cursor.nodes to checks if has nodes (is a folder) and .open to check it's open
+					if node_at_cursor.nodes and node_at_cursor.open then
+						-- .open.edit() toggles the node (folder) from open to closed
+						api.node.open.edit()
+					else
+						-- if instead node_at_cursor is a file or a closed folder, jump to the parent node (folder)
+						api.node.navigate.parent()
+					end
+				end
+				local righty = function()
+					local node_at_cursor = api.tree.get_node_under_cursor()
+					-- node_at_cursor.nodes to checks if has nodes (is a folder) and not .open to check it's closed
+					if node_at_cursor.nodes and not node_at_cursor.open then
+						-- .open.edit() toggles the node (folder) from closed to open
+						api.node.open.edit()
+						-- no else here, if it's an open folder or a file do nothing
+					end
+				end
+
+				vim.keymap.set("n", "h", lefty, { buffer = bufnr, desc = "Fold folder or go to parent" })
+				vim.keymap.set("n", "<Left>", lefty, { buffer = bufnr, desc = "Fold folder or go to parent" })
+				vim.keymap.set("n", "l", righty, { buffer = bufnr, desc = "Unfold folder" })
+				vim.keymap.set("n", "<Right>", righty, { buffer = bufnr, desc = "Unfold folder" })
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		vim.keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Toggle file explorer on current file" }
+		)
+		vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+	end,
 }
-
