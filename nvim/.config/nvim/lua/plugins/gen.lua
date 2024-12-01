93d2595109367b6cbd60300dd6c910e218d40486
Philippe Gouin <bering@ringlogic.com>
2024-11-30 19:33:38 -0500

    formatting

--- nvim/.config/nvim/lua/plugins/gen.lua
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -2,11 +2,10 @@ return {
   "David-Kunz/gen.nvim",
 
   keys = {
-    { "<leader>ai", "<cmd>Gen<CR>", desc="gen.nvim" },
+    { "<leader>ai", "<cmd>Gen<CR>", desc = "gen.nvim" },
   },
 
   opts = {
     model = "qwen2.5-coder",
-  }
+  },
 }
-
