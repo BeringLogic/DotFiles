c798e910a48c52f8fb7fe1e255353789cf64646e
Philippe Gouin <bering@ringlogic.com>
2024-11-30 19:32:13 -0500

    gen.nvim with qwen2.5-coder

--- nvim/.config/nvim/lua/plugins/gen.lua
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -2,7 +2,11 @@ return {
   "David-Kunz/gen.nvim",
 
   keys = {
-    { "<leader>ai", "<cmd>Gen<CR>", desc="Gen" },
+    { "<leader>ai", "<cmd>Gen<CR>", desc="gen.nvim" },
   },
+
+  opts = {
+    model = "qwen2.5-coder",
+  }
 }
 
