150034b417fa0f182fdfa1462c76d93542c1eb13
Philippe Gouin <bering@ringlogic.com>
2024-12-22 17:05:25 -0500

    hop.nvim

---
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -0,0 +1,11 @@
+return {
+	"smoka7/hop.nvim",
+	version = "*",
+	opts = {
+		keys = "etovxqpdygfblzhckisuran",
+	},
+	keys = {
+		{ "f", "<cmd>HopChar1<cr>", desc = "Hop Char1" },
+		{ "t", "<cmd>HopNodes<cr>", desc = "Hop Nodes" },
+	},
+}
