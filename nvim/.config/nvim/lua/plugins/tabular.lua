acf380e7851df73ec1720e9d23c761b828c39d48
Philippe Gouin <bering@ringlogic.com>
2025-03-04 21:44:08 -0500

    tabular nvim plugin to align

---
+++ nvim/.config/nvim/lua/plugins/tabular.lua
@@ -0,0 +1,8 @@
+return {
+  "godlygeek/tabular",
+  keys = {
+    { "<leader>a=",  "<cmd>Tabularize /=<cr>",      desc = "Align on =" },
+    { "<leader>a:",  "<cmd>Tabularize /:\\zs<cr>",  desc = "Align on :" },
+    { "<leader>a,",  "<cmd>Tabularize /,\\zs<cr>",  desc = "Align on ," },
+  }
+}
