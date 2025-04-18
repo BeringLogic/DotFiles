f205072c3116c987a1af4978faabc64925c3b2b5
Philippe Gouin <bering@ringlogic.com>
2025-04-18 08:56:56 -0400

    those are defaults with latest nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,9 +4,3 @@ vim.keymap.set("n", "<Leader>gs", "<CMD>setlocal spell!<CR>", { desc="Toggle Spe
 
 vim.keymap.set("n", "<leader><leader>", "<cmd>e #<CR>", { desc = "Previous File" })
 
-vim.keymap.set("n", "]b", "<cmd>bnext<CR>", { desc = "Next Buffer" })
-vim.keymap.set("n", "[b", "<cmd>bprev<CR>", { desc = "Previous Buffer" })
-
-vim.keymap.set("n", "]q", "<cmd>cnext<CR>", { desc = "Next QuickFix" })
-vim.keymap.set("n", "[q", "<cmd>cprev<CR>", { desc = "Previous QuickFix" })
-
