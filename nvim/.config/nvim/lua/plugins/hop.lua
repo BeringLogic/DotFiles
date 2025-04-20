826aa8c4abffb52d0cdee5731c01852fe08a95c5
Philippe Gouin <bering@ringlogic.com>
2025-04-20 17:44:40 -0400

    more standard f, F, t and T

--- nvim/.config/nvim/lua/plugins/hop.lua
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -5,7 +5,9 @@ return {
 		keys = "etovxqpdygfblzhckisuran",
 	},
 	keys = {
-		{ "f", "<cmd>HopChar1<cr>", desc = "Hop Char1" },
-		{ "t", "<cmd>HopNodes<cr>", desc = "Hop Nodes" },
+		{ "f", function() require("hop").hint_char1({ direction = require("hop.hint").HintDirection.AFTER_CURSOR }) end, desc = "Hop Char1 forward" },
+		{ "F", function() require("hop").hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR }) end, desc = "Hop Char1 backwards" },
+		{ "t", function() require("hop").hint_char1({ direction = require("hop.hint").HintDirection.AFTER_CURSOR, hint_offset = -1 }) end, desc = "Hop Char1 forward -1" },
+		{ "T", function() require("hop").hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR, hint_offset = 1 }) end, desc = "Hop Char1 backwards +1" },
 	},
 }
