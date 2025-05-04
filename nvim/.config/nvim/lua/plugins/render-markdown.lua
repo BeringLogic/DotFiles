cfedd7b7ae3218060427e6d5f9da68a2b6f797e4
Philippe Gouin <bering@ringlogic.com>
2025-05-04 11:06:37 -0400

    formatting

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,6 +1,9 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
   opts = {
     completions = { lsp = { enabled = true } },
     anti_conceal = { enabled = false },
