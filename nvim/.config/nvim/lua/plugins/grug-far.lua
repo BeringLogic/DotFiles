734664b689262ccc7b70c1780e3a853273ac929d
Philippe Gouin <bering@ringlogic.com>
2025-05-06 21:36:46 -0400

    grug-far

---
+++ nvim/.config/nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,11 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+  },
+  keys = {
+    { "<leader>g/", "<CMD>GrugFar<CR>", desc = "Grug Far" },
+  },
+}
