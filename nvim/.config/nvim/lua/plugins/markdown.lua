eb3d8324aaf4c52dcb8e270b0119ab8d08c3852f
Philippe Gouin <bering@ringlogic.com>
2025-05-07 21:20:37 -0400

    markdown-preview

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      completions = { lsp = { enabled = true } },
+      anti_conceal = { enabled = false },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    ft = { "markdown" },
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+  },
+}
