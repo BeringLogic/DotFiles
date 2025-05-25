0fc58617dbfd9fcea938d852a66c5423d17811df
Philippe Gouin <bering@ringlogic.com>
2025-05-25 13:41:56 -0400

    removed comments about sources since it's not true anymore

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,3 @@
-
--- Config mostly taken from https://github.com/cpow/neovim-for-newbs/tree/main
--- https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn
-
--- and https://github.com/josean-dev/dev-environment-files/tree/main?tab=readme-ov-file
--- https://www.youtube.com/watch?v=6pAG3BHurdM&t=2589s
-
 require("config/options")
 require("config/keymaps")
 
