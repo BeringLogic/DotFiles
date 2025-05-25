140f244e569f21c11d737dee2d7ca844931333bd
Philippe Gouin <bering@ringlogic.com>
2025-05-25 10:55:22 -0400

    alt-up/down in visual mode to move selection up/down one line

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -17,3 +17,8 @@ vim.keymap.set("n", "]t", "<CMD>tabnext<CR>", { desc = "Next Tab" })
 
 vim.keymap.set("n", "<leader>td", "<CMD>e todo.md<CR>", { desc = "Edit todo.md" })
 
+vim.keymap.set("v", "<A-down>", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "<A-up>", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+
