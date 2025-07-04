c0691015ac56eb029802c5267d17ab341f090620
Philippe Gouin <bering@ringlogic.com>
2025-07-04 18:34:02 -0400

    < and > in visual mode indents but keeps selection

--- nvim.main/.config/nvim/lua/config/keymaps.lua
+++ nvim.main/.config/nvim/lua/config/keymaps.lua
@@ -22,3 +22,5 @@ vim.keymap.set("v", "<A-up>", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
+vim.keymap.set("v", "<", "<gv", { desc = "Indent line left" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent line right" })
