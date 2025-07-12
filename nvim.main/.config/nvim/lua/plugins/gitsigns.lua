a4821d1690d13f670aaeff73d0bea80410e4d95c
Philippe Gouin <bering@ringlogic.com>
2025-07-12 15:30:07 -0400

    keymaps for gitsigns

--- nvim.main/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim.main/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,20 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', ']g', gitsigns.next_hunk, { desc = 'Next Git Hunk' })
+        map('n', '[g', gitsigns.prev_hunk, { desc = 'Previous Git Hunk' })
+        map('n', '<leader>gh', gitsigns.preview_hunk_inline, { desc = 'Preview Git Hunk' })
+      end
+    })
   end,
 }
