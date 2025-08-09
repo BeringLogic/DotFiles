80c81fec8aac3ef3238beff400439b1a8daad82c
Philippe Gouin <bering@ringlogic.com>
2025-08-09 00:30:21 -0400

    added screen off option to system menu

--- bin/.local/bin/rofi-system-menu.sh
+++ bin/.local/bin/rofi-system-menu.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+screenOff='󱗤 Screen Off'
 lock=' Lock'
 suspend='󰒲 Suspend'
 exit='󰍃 Exit'
 reboot='󰑓 Reboot'
 halt=' Halt'
 
-answer=$(echo -e "$lock\n$suspend\n$exit\n$reboot\n$halt" | rofi -dmenu -i -p "System menu" -theme-str 'window { width: 500px; }' -no-fixed-num-lines 5)
+answer=$(echo -e "$screenOff\n$lock\n$suspend\n$exit\n$reboot\n$halt" | rofi -dmenu -i -p "System menu" -theme-str 'window { width: 500px; }' -no-fixed-num-lines 5)
 
 case "$answer" in
+  "$screenOff")
+    niri msg action power-off-monitors
+    ;;
   "$lock")
     loginctl lock-session
     ;;
