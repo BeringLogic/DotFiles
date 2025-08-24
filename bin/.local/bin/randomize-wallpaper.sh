10747f172be3e446056191c64ae613f43116966d
Philippe Gouin <bering@ringlogic.com>
2025-08-24 13:05:10 -0400

    removed wallpapers from dotfiles repo

--- bin/.local/bin/randomize-wallpaper.sh
+++ bin/.local/bin/randomize-wallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-swww img $HOME/Images/wallpapers/$(ls $HOME/Images/wallpapers/ | shuf -n 1)
+swww img $HOME/Data/Pictures/Wallpapers/$(ls $HOME/Data/Pictures/Wallpapers | shuf -n 1)
 
